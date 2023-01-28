eb883c89e7ab8dff977bdb05b69ecc0402e90ce8
miladghasemidev <milad_ghasemi@hotmail.com>
2023-01-28 14:36:23 +0330

    Save comments into CSV

---
+++ Code/Save comments into CSV.py
@@ -0,0 +1,81 @@
+from csv import DictWriter
+import requests
+from typing import List, TypedDict
+
+# Type definitions
+class Reactions(TypedDict):
+    likes: int
+    dislikes: int
+
+
+class Comment(TypedDict):
+    id: int
+    title: str
+    body: str
+    created_at: str
+    rate: int
+    reactions: Reactions
+    is_buyer: bool
+    user_name: str
+    is_anonymous: bool
+
+
+def save_comments_to_exel(json_data, filename):
+    keys = []
+    for i in range(len(json_data)):
+        try:
+            sub_obj = json_data[i]
+            if i == 0:
+                keys = list(sub_obj.keys())
+        except:
+            pass
+
+    with open(filename, 'w',encoding='UTF-8',newline='') as csvfile:
+        csvwriter = DictWriter(csvfile,fieldnames = keys)
+        csvwriter.writeheader()
+        csvwriter.writerows(json_data)
+    csvfile.close()
+
+def get_comment(sess: requests.Session, product_id: int, page: int = 1, pager: bool = False) -> List[Comment]:
+    URL = f'https://api.digikala.com/v1/product/{product_id}/comments/?page={page}'
+    response = sess.get(URL)
+
+    if response.ok:
+        data = response.json().get('data')
+        complete_comment = data.get('comments')
+
+        comments = []
+        for comment in complete_comment:
+            comments.append({
+                            'productID' : product_id,
+                            'id': comment.get('id'),
+                            'title': comment.get('title'),
+                            'body': comment.get('body'),
+                            'created_at': comment.get('created_at'),
+                            'rate': comment.get('rate'),
+                            'likes': comment.get('reactions').get('likes'),
+                            'dislikes':comment.get('reactions').get('dislikes'),
+                            'recommendation_status': comment.get('recommendation_status'),
+                            'is_buyer': comment.get('is_buyer'),
+                            'user_name': comment.get('user_name'),
+                            'is_anonymous': comment.get('is_anonymous'),
+                            })
+
+        if pager:
+            return  comments, data.get('pager')
+        return comments
+    return []
+
+
+if __name__ == '__main__':
+    product_id = 3493882
+    
+    sesstion = requests.Session()
+    filename = "D:\Programming\ML_projects\OutputFiles\ " +  str(product_id)  + ".csv"
+    comments, pager = get_comment(sesstion, product_id, pager=True)
+    total_pages = pager.get('total_pages')
+
+    for page in range(1, total_pages + 1):
+        comment = get_comment(sesstion, product_id, page=page)
+        comments.extend(comment)
+    save_comments_to_exel(comments, filename)
