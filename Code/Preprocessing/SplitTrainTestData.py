0938a16ae9a750392c2a41a6537702147671fc60
miladghasemidev <milad_ghasemi@hotmail.com>
2023-02-07 19:38:02 +0330

    Save specific columns into new file

--- Code/Preprocessing/SplitTrainTestData.py
+++ Code/Preprocessing/SplitTrainTestData.py
@@ -9,9 +9,12 @@ NanRecords = pd.isnull(df['recommendation_status'])
 #Below line hold not Nan records
 NotNanRecords = pd.notnull(df['recommendation_status'])
 
+#Select the columns that want to be in new file
+header = ['productID','body', 'recommendation_status']
+
 #Write NaN and not NaN records to seperate files
-df[NanRecords].to_csv('D:\Programming\ML_projects\OutputFiles\Test\\3493882.csv')
-df[NotNanRecords].to_csv('D:\Programming\ML_projects\OutputFiles\Train\\3493882.csv')
+df[NanRecords].to_csv('D:\Programming\ML_projects\OutputFiles\Test\\3493882.csv', columns = header)
+df[NotNanRecords].to_csv('D:\Programming\ML_projects\OutputFiles\Train\\3493882.csv', columns = header)
 
 #After split the original file below code will remove base file
 os.remove('D:\Programming\ML_projects\OutputFiles\\3493882.csv')
